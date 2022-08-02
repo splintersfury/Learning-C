bdffb14bda935c76b3b63407e5f4a3dd7c74afec
splintersfury <ahmadabdillah@live.com>
2022-08-02 15:31:07 +0800

    Completed Windows 2

---
+++ Windows_Api/README.md
@@ -0,0 +1,3 @@
+I realised that the assignment 24 deals with linux calls whereas I am more interested interfacing with windows.
+
+This portion will delve into interacting with windows api.
---
+++ Windows_Api/Windows-1.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+int WINAPI WinMain (HINSTANCE hInstance, 
+HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow)
+{
+MessageBox (NULL, "This is just the beginning", "Start", 0) ;
+return 0 ;
+}
